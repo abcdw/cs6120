efc612f31fe2a1f1c43c458c09a0ad01991de581
Andrew Tropin <andrew@trop.in>
2025-02-19 10:28:10 +0400

    Implement naive dce pass and infrastructure to run transformations

--- src/guile/cs6120/core.scm
+++ src/guile/cs6120/core.scm
@@ -79,13 +79,13 @@
        '(() #f)
        blocks)))))
 
-(define (comment)
-  (define bril-program
-    (call-with-input-file
-        "/data/abcdw/work/sampsyo/bril/tmp/jmp.json"
-      (lambda (port)
-        (json->scm port))))
+(define bril-program
+  (call-with-input-file
+      "/data/abcdw/work/sampsyo/bril/tmp/jmp.json"
+    (lambda (port)
+      (json->scm port))))
 
+(define (print-bril-blocks bril-program)
   (vector-map
    (lambda (i x)
      (format #t "=============\n")
@@ -96,13 +96,118 @@
    (assoc-ref bril-program "functions"))
   "hi")
 
+(define (function->blocks bril-function)
+  (instructions->blocks (function-instructions bril-function)))
+
+(define (get-blocks bril-program)
+  (vector-map
+   (lambda (i x) (function->blocks x))
+   (assoc-ref bril-program "functions")))
+
+(define (blocks->instrs blocks)
+  (list->vector
+   (append-map cdr (car blocks))))
+
+(define* (alist-update alist key f #:optional (dflt '()))
+  (let* ((prev-pair (assoc key alist))
+         (prev (if prev-pair (cdr prev-pair) dflt)))
+    (acons
+     key
+     (f prev)
+     (alist-delete key alist equal?))))
+
+(define (transform-program-instrs bril-program instrs-transformation)
+  (define (update-functions fns)
+    (vector-map
+     (lambda (i x)
+       (alist-update x "instrs" instrs-transformation))
+     fns))
+  (alist-update bril-program "functions" update-functions))
+
+(define (update-blocks-instrs blocks function)
+  (cons
+   (map
+    (lambda (block)
+      (cons (car block) (function (cdr block))))
+    (car blocks))
+   (cdr blocks)))
+
+(define (remove-unused-variables block-instrs)
+  "Just removes the variables that never used for sure."
+  (define used-variables
+    (fold
+     (lambda (x acc)
+       (lset-union
+        equal?
+        acc
+        (vector->list (or (assoc-ref x "args") #()))))
+     '()
+     block-instrs))
+
+  (fold-right
+   (lambda (x acc)
+     (if (and (assoc "dest" x)
+              (not (member (assoc-ref x "dest") used-variables)))
+         acc
+         (cons x acc)))
+   '()
+   block-instrs))
+
+(define (tdce instrs)
+  (define blocks (instructions->blocks instrs))
+  ;; (map
+  ;;  (lambda (x) ((@ (ice-9 pretty-print) pretty-print) x))
+  ;;  (car blocks))
+  (blocks->instrs (update-blocks-instrs blocks remove-unused-variables)))
+
+;; ((@ (ice-9 pretty-print) pretty-print)
+;;  (transform-program-instrs tdce-combo-bril tdce))
+
+;; (define (edges->adjacency-list edges)
+;;   (define (add-vertex v)
+;;     (lambda (l)
+;;       (cons v l)))
+;;   (reverse
+;;    (map
+;;     (lambda (x)
+;;       (cons
+;;        (car x)
+;;        (reverse (cdr x))))
+;;     (fold
+;;      (lambda (x acc)
+;;        (update-alist acc (car x) (add-vertex (cdr x)) '()))
+;;      '()
+;;      edges))))
+
+;; (define (topolgical-sort adjacency-list))
+
+
+;; (update-alist '((key . (hi)) (key2 . (hello)))
+;;               'key (lambda (x) '(hello there)) '())
+
+
+;; (display (blocks->bril-program
+;;           (vector-ref (get-blocks bril-program) 0)))
+;; (newline)
+
+(define (comment)
+  (print-blocks tdce-combo-bril)
+  (get-blocks bril-program)
+
+  (define tdce-combo-bril
+    (call-with-input-file
+        "./src/bril/tdce/combo.json"
+      (lambda (port)
+        (json->scm port))))
+  "he")
 
 (define-public (identity-transformation)
   (define input (get-string-all (current-input-port)))
   (display input))
 
 (define-public (tdce-transformation)
-  (identity-transformation))
+  (define program (json->scm (current-input-port)))
+  (scm->json (transform-program-instrs program tdce) (current-output-port)))
 
 ;; TODO: [Andrew Tropin, 2025-02-13] Use guix for creating a script,
 ;; which contains pipeline and can be re-executed from repl
